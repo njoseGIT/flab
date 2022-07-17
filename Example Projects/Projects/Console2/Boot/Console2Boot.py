3cb017c1f58d5367e3224d1a9a214762701d92ff
njoseGIT <53254221+njoseGIT@users.noreply.github.com>
2022-07-17 20:03:28 +0100

    2.0.1 compatible

--- Example Projects/Projects/Console2/Boot/Console2Boot.py
+++ Example Projects/Projects/Console2/Boot/Console2Boot.py
@@ -1,7 +1,7 @@
 #Console2Boot
-#A boot script for an interactive FLab development application - FLab 2
+#A boot script for an interactive Flab development application - Flab 2
 #Compatible with Python 3.9+
-#Requires installation of PyQt5 v.5.15+ and flab v.1.1.1+
+#Requires installation of PyQt5 v.5.15+ and flab v.1.1.2
 #Distributed under GNU GPL v3
 #Nicholas A. Jose
 #Feb 2022
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         ui_queue = boot_manager.create_queue()
         flab_queue = boot_manager.create_queue()
 
-        f = boot_manager.create_flab_proxy(ui_queue, flab_queue)
+        f = boot_manager.create_flab_proxy(ui_queue = ui_queue, flab_queue = flab_queue, print_status = False)
 
         f.tasks.update({'Console2UIProcess':Console2UIProcess.Task(f)})
         f.tasks.update({'ConsoleFlabProcess':ConsoleFlabProcess.Task(f)})
